f735d265758e44a0c9b9a7b9d62863750724f4f0
Carsten König <carstenkoenig92@gmail.com>
2025-01-25 18:23:04 +0100

    next upgrade

--- src/components/pages/home/DesktopApp/Download.tsx
+++ src/components/pages/home/DesktopApp/Download.tsx
@@ -2,10 +2,10 @@ import { userAgentOs, sortedOs, Os } from '@/helpers/os'
 import DownloadWindows from './DownloadWindows'
 import DownloadLinux from './DownloadLinux'
 import DownloadMac from './DownloadMac'
-import { headers } from 'next/headers'
+import { headers } from 'next/headers';
 
-export default function DesktopAppDownload() {
-  const ua = headers().get('User-Agent')
+export default async function DesktopAppDownload() {
+  const ua = (await headers()).get('User-Agent')
   const os = userAgentOs(ua)
   const sorted = sortedOs(os)
 
